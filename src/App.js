dfc6fdcbd7dfdde90355c2ece579977a825c1a72
Stefan Dosch <sd@Stefans-MacBook-Pro.local>
2019-10-08 22:56:23 +0200

    added sign-in-sing-up component, form-input component, sign-in-component

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.scss";
 
+import Header from "./components/header/header.component";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+import SignInAndSingUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSingUpPage} />
       </Switch>
     </div>
   );
